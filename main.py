5839eb47b9b138822bdf0cb99db36dfa5bfed58b
TheWhiteDuke1 <114252786+TheWhiteDuke1@users.noreply.github.com>
2023-09-05 16:40:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def text(message: types.Message):
     data = json.loads(response.text)
 
     # Send the formatted data to the user on Telegram
-    await bot.send_message(message.chat.id,f"""
+    await bot.send_message(message.chat.id,f""
                            
                            👨 ФИО: {data['name']}
                            🏳️ Страна: {data['country']}
